07b0f1fbda2ce49f0a642aade7359fd65caa9242
wahid1099 <wahidahmed890@gmail.com>
2023-02-18 23:06:41 +0600

    adding about us page

--- home_app/urls.py
+++ home_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index,about
 
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("aboutus", about, name="about")
 ]

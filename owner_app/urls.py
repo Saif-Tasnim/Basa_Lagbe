0365234cb26b0e477902a1356dcd465b1cd52e6a
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-02-23 13:08:22 +0600

    owner buy features with (Flat & Land) has done

--- owner_app/urls.py
+++ owner_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ownerDash
+from .views import ownerDash,ownerBuy,ownerSell
 
 urlpatterns = [
-    path("", ownerDash, name="dash")
+    path("", ownerDash, name="dash"),
+    path("Owner-Buy",ownerBuy,name="buy"),
+    path("Owner-Sell",ownerSell, name="sell")
    
 ]

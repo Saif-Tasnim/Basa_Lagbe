4487e47fd395d21a1eeb74bcb4dcba65361ee1a5
wahid1099 <wahidahmed890@gmail.com>
2023-03-03 11:57:31 +0600

    adding alerts

--- user_app/views.py
+++ user_app/views.py
@@ -4,7 +4,7 @@ from .models import OwnerRent
 from django.contrib import messages
 import time
 
-
+import os
 # Create your views here.
 
 # this "identifyUser" view identyfies whether the user is a land owner or a normal user.
@@ -131,12 +131,19 @@ def owner_post(request):
 
 def delete_post(request,id):
     OwnerRent.objects.get(id=id).delete()
+    messages.success(request,"Post deleted Sucesfully!!")
+
     time.sleep(1)
+
     return render(request, "OwnerDashboard/owner_post.html")
 
 def updatepost(request,id):
     single_post= OwnerRent.objects.get(id=id)
     if request.method == "POST":
+        if len(request.FILES) !=0:
+            if single_post.rent_photo:
+                os.remove(single_post.rent_photo.path)
+            single_post.rent_photo=request.FILES['rentimage']    
         get_method = request.POST.copy()
         single_post.property_type = request.POST.get("property_type")
         single_post.rent_type = request.POST.get("rent_type")

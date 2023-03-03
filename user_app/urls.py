83a4ed42e05718dc5a543836a92fb0c3e9d65e9f
wahid1099 <wahidahmed890@gmail.com>
2023-03-03 11:30:40 +0600

    adding update delete

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import (identifyUser, ownerBuy, ownerDashboard, ownerRent,
-                    ownerSell, userBuy, userDashboard, userRent, userShow,owner_post,delete_post)
+                    ownerSell, userBuy, userDashboard, userRent, userShow,owner_post,delete_post,updatepost)
 
 urlpatterns =[
     path("", identifyUser, name="identify-user"),
@@ -14,5 +14,6 @@ urlpatterns =[
     path("owner-sell",ownerSell, name="sell"),
     path("owner-rent",ownerRent, name="rent"),
     path("owner-post",owner_post, name="posts"),
-    path("post_delete/<int:id>/",delete_post, name="deletepost")
+    path("post_delete/<int:id>/",delete_post, name="deletepost"),
+    path("update/<int:id>/",updatepost, name="updatepost")
 ]

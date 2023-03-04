c810d969f4f05ce5d80f31b53450336ff79363fb
Saad-001 <saadafnan21@gmail.com>
2023-03-04 12:22:46 +0600

    owner-dashboard UI updated

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import OwnerRent, Sell_flat, Sell_land
+
 # Register your models here.
+
+admin.site.register(Sell_flat)
+admin.site.register(Sell_land)
+admin.site.register(OwnerRent)

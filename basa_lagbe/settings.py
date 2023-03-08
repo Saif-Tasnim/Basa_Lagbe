de1c384cf8dfa92f0089046f82601248b7944c5f
wahid1099 <wahidahmed890@gmail.com>
2023-03-08 14:03:37 +0600

    adding user contact email send

--- basa_lagbe/settings.py
+++ basa_lagbe/settings.py
@@ -161,3 +161,12 @@ CORS_ALLOWED_ORIGINS = [
 AUTH_USER_MODEL = 'account.NewUser'
 # Authentication backends
 AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER = 'mw0641295@gmail.com'
+EMAIL_HOST_PASSWORD = 'suaqwcgzbdyvmgwn'
+EMAIL_USE_TLS=True
